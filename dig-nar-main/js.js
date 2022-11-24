ad1d6c288be13b345d06330ca6b5efee22e390bb
Lailabc <115160711+Lailabc@users.noreply.github.com>
2022-11-24 11:01:28 +0000

    Add files via upload

---
+++ dig-nar-main/js.js
@@ -0,0 +1,40 @@
+const observer = new IntersectionObserver ((entries)=>{
+  entries.forEach((entry) =>{
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add("show");
+    }else {
+      entry.target.classList.remove("show");
+    }
+  })
+})
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+var video = document.getElementById("egg")
+
+window.addEventListener("scroll", function() {
+    video.style.transform = "scale(1.2, 1.5)";
+});
+
+
+// const buttonContainer = document.getElementById("first")
+// buttonContainer.addEventListener("click",revealimg)
+// const images = document.getElementsByClassName("images")
+
+// let imageIndex = 0;
+// function revealimg() {
+//   imageIndex++
+//   for(let i=0; i<images.length; i++){
+//     if(i=== imageIndex){
+//       images[i].style.display="block"
+//     } else{
+//       images[i].style.display="none"
+
+//     }
+//   }if(images.length ===[i>9] ){
+//     const x= getElementById(p)
+//     x.style.display="block"
+//   }
+// }
+
